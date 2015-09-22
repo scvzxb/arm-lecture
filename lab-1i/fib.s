f85adc33992e3b04d21a6fd8799baa762db23f28
scvzxb <scvzxb@gmail.com>
2015-09-22 17:23:28 +0800

    First commit

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,44 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3, r4, r5, r6, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	sub		r3, r0, #0	@R3 =>  n
+	cmp 	r0, #0
+	ble  	.L3
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	cmp		r0, #1
+	ble 	.L4
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
 
-	@ R0 = R5 + R0 (update flags)
 
-	pop {r3, r4, r5, pc}		@EPILOG
+	mov 	r4, #2   @use R4 as i
+	mov		r5, #1	@previous
+	mov  	r3, #0  @result
+	mov		r6, #0 @previous#2
+
+	.loop:
+	add 	r4, r4, #1
+	add 	r3, r5, r6
+	mov 	r6, r5
+	mov		r5, r3
+	cmp 	r4,	r0
+	ble .loop
+	mov 	r0, r3
+
+
+
+	pop {r3, r4, r5, r6, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r3, r4, r5, r6, pc}		@ EPILOG
 
 .L4:
 	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r3, r4, r5, r6,pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
