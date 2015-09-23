a6770747ee00cd28089ce719ee599fdc557e3be1
scvzxb <scvzxb@gmail.com>
2015-09-23 15:49:27 +0800

    secnod commit

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,7 +11,7 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, r6, lr}
+	push {r3, r5, r6, lr}
 
 
 	sub		r3, r0, #0	@R3 =>  n
@@ -23,32 +23,31 @@ fibonacci:
 
 
 
-	mov 	r4, #2   @use R4 as i
 	mov		r5, #1	@previous
 	mov  	r3, #0  @result
-	mov		r6, #0 @previous#2
+	mov		r6, #0  @previous#2
 
 	.loop:
-	add 	r4, r4, #1
+	sub 	r0, r0, #1
 	add 	r3, r5, r6
 	mov 	r6, r5
 	mov		r5, r3
-	cmp 	r4,	r0
-	ble .loop
+	cmp 	r0,	#2
+	bne .loop
 	mov 	r0, r3
 
 
 
-	pop {r3, r4, r5, r6, pc}		@EPILOG
+	pop {r3, r5, r6, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, r6, pc}		@ EPILOG
+	pop {r3, r5, r6, pc}		@ EPILOG
 
 .L4:
 	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, r6,pc}		@ EPILOG
+	pop {r3, r5, r6,pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
